1cb366f3af241fadcec5bf39ebe80acd3482ddf0
Pablo Nieves <53600701+pnieves2019@users.noreply.github.com>
2020-09-14 14:00:01 +0200

    Update

--- maelasviewer.py
+++ maelasviewer.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# Run this app with `python app.py` and
+# Run this app with `python3 maelasviewer.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import pandas as pd
-import dash_table
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
